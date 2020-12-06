bd4f52d6a83c1a69c61e5e3bd94887072df494fb
greghxc <greg@hacksy.io>
2020-12-05 22:27:49 -0800

    little saint nick

--- src/main/java/io/hacksy/aoc/v2020/day06/Day06Processor.java
+++ src/main/java/io/hacksy/aoc/v2020/day06/Day06Processor.java
@@ -2,18 +2,17 @@ package io.hacksy.aoc.v2020.day06;
 
 import io.vavr.Tuple;
 import io.vavr.collection.CharSeq;
-import io.vavr.collection.List;
 
 public class Day06Processor {
     int partOne(String input) {
         return CharSeq.of(input).split("\\n\\n")
-                .map(a -> List.ofAll(a.toCharArray()).filter(c -> c != '\n').toSet().size())
+                .map(a -> CharSeq.of(a).filter(c -> c != '\n').toSet().size())
                 .sum().intValue();
     }
 
     int partTwo(String input) {
         return CharSeq.of(input).split("\\n\\n")
-                .map(s -> List.ofAll(s.toCharArray()).groupBy(c -> c).toMap(t -> Tuple.of(t._1, t._2.size())))
+                .map(s -> CharSeq.of(s).groupBy(c -> c).toMap(t -> Tuple.of(t._1, t._2.size())))
                 .map(m -> m.values().filter(v -> v == m.getOrElse('\n', 0) + 1).size())
                 .sum().intValue();
     }
