09dcfc39ede2fe7b68068133a514c1829c91e8ad
greghxc <greg@hacksy.io>
2020-12-05 22:13:17 -0800

    little saint nick

--- src/main/java/io/hacksy/aoc/v2020/day06/Day06Processor.java
+++ src/main/java/io/hacksy/aoc/v2020/day06/Day06Processor.java
@@ -7,13 +7,13 @@ import java.util.Arrays;
 
 public class Day06Processor {
     int partOne(String input) {
-        return List.ofAll(Arrays.asList(input.split("(?s)\\n\\n")))
+        return List.ofAll(Arrays.asList(input.split("\\n\\n")))
                 .map(a -> List.ofAll(a.toCharArray()).filter(c -> c != '\n').toSet().size())
                 .sum().intValue();
     }
 
     int partTwo(String input) {
-        return List.ofAll(Arrays.asList(input.split("(?s)\\n\\n")))
+        return List.ofAll(Arrays.asList(input.split("\\n\\n")))
                 .map(s -> List.ofAll(s.toCharArray()).groupBy(c -> c).toMap(t -> Tuple.of(t._1, t._2.size())))
                 .map(m -> m.values().filter(v -> v == m.getOrElse('\n', 0) + 1).size())
                 .sum().intValue();
